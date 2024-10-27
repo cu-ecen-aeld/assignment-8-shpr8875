d8dc1852b603d282003b29d1005a00364cead654
shweta <shweta.prasad@colorado.edu>
2024-10-27 16:56:22 -0600

    Assignment 8 final commit, driver test passing on local but fail on full test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,11 @@
 #Assignment5 part 2:
 #AESD_ASSIGNMENTS_VERSION = '1644e63cb2b86209b3cd4b393984165a32a23c70'
 #AESD_ASSIGNMENTS_VERSION = '0915b936e72d192d5a76ada1e789e7b665baa88c'
-AESD_ASSIGNMENTS_VERSION = '0ee0cb70c8b824dbef03cfd10d6ffb978262e133'
+#AESD_ASSIGNMENTS_VERSION = '0ee0cb70c8b824dbef03cfd10d6ffb978262e133'
+
+#Assignment8:
+AESD_ASSIGNMENTS_VERSION = '1d2824c1b49adf4b87582776d3bb19fc9e8ff5ac'
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,6 +27,12 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
+
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
+
+
 # add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	#$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
@@ -34,6 +44,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	#$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/root
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
     	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+  	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/sbin/
+    	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/sbin/  	
+    	
+    	
 	
 endef
 
